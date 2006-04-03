22afc59a56771c88ecb37a01eac5cb0dc0e480ce
cvs2git <cvs2git>
2006-04-03 00:59:00 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_TP'.

--- portland/dapi/generic/commands.h
+++
@@ -1,4 +0,0 @@
-int openUrl( const char* url );
-int suspendScreensaving( Display* dpy, int suspend );
-int mailTo( const char* subject, const char* body, const char* to,
-    const char* cc, const char* bcc, const char** attachments, int att_count );
