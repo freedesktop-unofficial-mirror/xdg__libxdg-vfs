9ed2ddd27199c93e1977b91523e124254f54adf8
cvs2git <cvs2git>
2006-10-09 22:53:06 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE_1_0'.

--- xdg-vfs-browser-demo/setpaths.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-export PATH=/home/norbert/work/c/xdg-vfs-kde/src/:$PATH
-export PATH=/home/norbert/work/c/xdg-vfs-gnome/src/:$PATH
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-
-
